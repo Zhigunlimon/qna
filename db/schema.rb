383263b2e34c4eb555fe292a730ffe047742b442
Logical Groove <info@logicalgroove.com>
2016-01-24 17:53:08 +0000

    added model Answer that belongs to question

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120112044) do
+ActiveRecord::Schema.define(version: 20160124174621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
---
+++ spec/factories/answers.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :answer do
+    body "MyText"
+question_id 1
+  end
+
+end
