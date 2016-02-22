1bf27f8dc4875a853668bd682b1b91a69d4be2e1
Logical Groove <info@logicalgroove.com>
2016-02-22 10:59:47 +0000

    lesson 5 adding answers by js

--- spec/features/answers/create_answer_spec.rb
+++ spec/features/answers/create_answer_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
-feature 'Create question' do
+feature 'Create answer' do
   let(:user) { create(:user) }
   let!(:question) { create(:question, user: user) }
 
-  scenario 'Authenticated user create answer to the question' do
+  scenario 'Authenticated user create answer to question', js: true do
     sign_in(user)
 
     visit question_path(question)
     fill_in 'answer[body]', with: 'body for the answer'
     click_on 'Post answer'
 
-    expect(page).to have_content 'Your answer was successfully created.'
-    expect(page).to have_content 'body for the answer'
+    expect(current_path).to eq question_path(question)
+    within '.answers' do
+      expect(page).to have_content 'body for the answer'
+    end
   end
 
   scenario 'Non-authenticated user try to create answer' do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,9 +6,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'factory_girl_rails'
+require 'capybara/rspec'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
+Capybara.javascript_driver = :webkit
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
@@ -16,12 +17,33 @@ RSpec.configure do |config|
 
   config.include Devise::TestHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
-  config.include AcceptanceMacros, type: :feature
-
-  config.use_transactional_fixtures = true
-  config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
+  config.include AcceptanceMacros, type: :feature
+  config.include Capybara::DSL, type: :feature
 
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+  config.render_views
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
 Shoulda::Matchers.configure do |config|
